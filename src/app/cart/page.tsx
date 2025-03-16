7550adb1ba1f88cbeba13707f10fbc4d5dc04789
你的GitHub用户名 <vincercai@hotmail.com>
2025-03-16 18:15:00 +0700

    Fix CartContext import path in cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useCart } from '../contexts/CartContext';
+import { useCart } from '@/contexts/CartContext';
 import { useRouter } from 'next/navigation';
 
 export default function Cart() {
